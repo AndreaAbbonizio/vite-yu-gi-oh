c5aa3998217a74b8d2fd6339e0576f44f1c9d9dc
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-03-28 14:43:24 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container-header">
+        <h1>Yu-Gi-Oh Cards</h1>
+    </div>
+</template>
+
+<style scoped>
+.container-header {
+    padding: 10px;
+    text-align: center;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+
+<template>
+    <div class="container-cards">
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.container-cards {
+    max-width: 1000px;
+    margin: 0 auto;
+}
+</style>
